036011596c017519c4a96177d20e137284f07a3e
yannikmity <yannik.gassmann@gmail.com>
2021-04-20 12:50:09 +0200

    hello world controller erstellt

---
+++ src/main/java/com/webtech/what2cook/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.webtech.what2cook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping
+    public String landingPage(){
+        return "Welcome";
+    }
+
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+}
