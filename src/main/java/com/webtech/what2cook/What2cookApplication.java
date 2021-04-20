da163a42e8b0aff1930f4494819bf6709ab0522c
yannikmity <46356751+yannikmity@users.noreply.github.com>
2021-04-20 11:06:58 +0200

    Add files via upload

---
+++ src/main/java/com/webtech/what2cook/What2cookApplication.java
@@ -0,0 +1,13 @@
+package com.webtech.what2cook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What2cookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(What2cookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webtech/what2cook/What2cookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.what2cook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What2cookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
